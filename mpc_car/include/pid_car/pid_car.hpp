2f8d8295e69bde8526ef150bb8d61b6d2e9f50ca
2B-people <nqq0915@163.com>
2024-07-20 22:34:32 +0800

    <ci>[pid_car]:完成基本的开发，明天测试

--- mpc_car/include/pid_car/pid_car.hpp
+++ mpc_car/include/pid_car/pid_car.hpp
@@ -5,6 +5,7 @@
 #include <ros/ros.h>
 
 #include <arc_spline/arc_spline.hpp>
+#include <tf/transform_datatypes.h>
 #include <deque>
 
 namespace mpc_car
@@ -24,6 +25,11 @@ namespace mpc_car
 
         int N_;
         double dt_;
+        double prev_error_;
+        double error_sum_;
+
+        double kp_;
+        double ki_;
 
         VectorS x0_observe_;
 
@@ -55,12 +61,18 @@ namespace mpc_car
 
             nh.getParam("desired_v", desired_v_);
             nh.getParam("dt", dt_);
+            nh.getParam("N", N_);
+            nh.getParam("Kp", kp_);
+            nh.getParam("Ki", ki_);
 
             ref_pub_ = nh.advertise<nav_msgs::Path>("reference_path", 1);
             ref_state_pub_ = nh.advertise<geometry_msgs::PoseStamped>("ref_state", 1);
+
+            prev_error_ = 0.0;
+            error_sum_ = 0.0;
         }
 
-        int solvePID(const VectorS &x0_observe)
+        int solvePID(const VectorS &x0_observe, double &pid_out)
         {
             x0_observe_ = compensateDelay(x0_observe); // compensate delay
             // set reference states
@@ -88,7 +100,47 @@ namespace mpc_car
                 last_phi = phi;
                 reference_states_[i] = Eigen::Vector3d(xy_r.x(), xy_r.y(), phi);
             }
-            return 0;
+            double ref_phi = reference_states_[N_ - 1](2);
+            double obs_phi = x0_observe_(2);
+            auto err = obs_phi - ref_phi;
+            error_sum_ += err;
+
+            // double pid_out = kp_ * err + ki_ * error_sum_;
+            pid_out = kp_ * err;
+
+            std::cout << "\033[32m"
+                      << "error: " << err << " " << "pid_out: " << pid_out
+                      << "\033[0m" << std::endl;
+            return 1;
+        }
+
+        void visualization()
+        {
+            nav_msgs::Path msg;
+            msg.header.frame_id = "world";
+            msg.header.stamp = ros::Time::now();
+            geometry_msgs::PoseStamped p;
+            for (double s = 0; s < s_.arcL(); s += 0.01)
+            {
+                p.header.stamp = ros::Time::now();
+                p.pose.position.x = s_(s).x();
+                p.pose.position.y = s_(s).y();
+                p.pose.position.z = 0.0;
+                msg.poses.push_back(p);
+            }
+            ref_pub_.publish(msg);
+            msg.poses.clear();
+
+            geometry_msgs::PoseStamped msg1;
+            msg1.header.stamp = ros::Time::now();
+            msg1.header.frame_id = "world";
+            msg1.pose.position.x = reference_states_[N_ - 1](0);
+            msg1.pose.position.y = reference_states_[N_ - 1](1);
+            msg1.pose.position.z = 0.0;
+            double ref_phi = reference_states_[N_ - 1](2);
+            geometry_msgs::Quaternion quat = tf::createQuaternionMsgFromYaw(ref_phi);
+            msg1.pose.orientation = quat;
+            ref_state_pub_.publish(msg1);
         }
     };
 } // namespace mpc_car
--- mpc_car/src/pid_car_nodelet.cpp
+++ mpc_car/src/pid_car_nodelet.cpp
@@ -2,6 +2,7 @@
 #include <nodelet/nodelet.h>
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
+#include <serial/serial.h>
 
 #include <Eigen/Geometry>
 #include <pid_car/pid_car.hpp>
@@ -15,11 +16,82 @@ namespace mpc_car
         ros::Timer plan_timer_;
         ros::Subscriber odom_sub_;
 
+        serial::Serial ser_;
+        bool use_serial_ = false;
+        std::string port_;
+        int baudrate_;
         VectorS state_;
+
+        void odom_call_back(const nav_msgs::Odometry::ConstPtr &msg)
+        {
+            // ROS_WARN("odom_call_back");
+            double x = msg->pose.pose.position.x;
+            double y = msg->pose.pose.position.y;
+            Eigen::Quaterniond q(msg->pose.pose.orientation.w,
+                                 msg->pose.pose.orientation.x,
+                                 msg->pose.pose.orientation.y,
+                                 msg->pose.pose.orientation.z);
+            Eigen::Vector3d euler = q.toRotationMatrix().eulerAngles(0, 1, 2);
+
+            state_ << x, y, euler.z();
+        }
 
         void plan_timer_callback(const ros::TimerEvent &event)
         {
-            ROS_INFO("plan_timer_callback");
+            int ret = 0;
+            double pid_out = 0;
+            ret = pidPtr_->solvePID(state_, pid_out);
+            assert(ret == 1);
+            if (use_serial_)
+            {
+                std::string send_data = "z" + std::to_string((int)(pid_out * 100)) + "\n";
+                u_char send_data_char[send_data.size()];
+                for (size_t i = 0; i < send_data.size(); i++)
+                    send_data_char[i] = send_data.c_str()[i];
+                ser_.write(send_data_char, send_data.size());
+            }
+            else
+            {
+                std::string send_data = "z" + std::to_string((int)(pid_out * 100)) + "\n";
+                std::cout << send_data;
+                // TODO,让仿真动起来
+            }
+
+            pidPtr_->visualization();
+        }
+
+        int SerialInit()
+        {
+            ser_.setPort(port_);
+            ser_.setBaudrate(baudrate_);
+            serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+            ser_.setTimeout(to);
+            ROS_WARN("---[param] serial set:%s, baudrate set:%d----", port_.c_str(), baudrate_);
+            try
+            {
+                ser_.open();
+            }
+            catch (serial::IOException &e)
+            {
+                ROS_ERROR_STREAM("Unable to open port ");
+                return 0;
+            }
+            if (ser_.isOpen())
+            {
+                ROS_INFO_STREAM("Serial Port initialized");
+            }
+            else
+            {
+                ROS_ERROR_STREAM("Serial Port fail");
+                return 0;
+            }
+            std::string send_data = "z000\n";
+            u_char send_data_char[send_data.size()];
+            for (size_t i = 0; i < send_data.size(); i++)
+                send_data_char[i] = send_data.c_str()[i];
+            ser_.write(send_data_char, send_data.size());
+
+            return 1;
         }
 
     public:
@@ -29,8 +101,20 @@ namespace mpc_car
             pidPtr_ = std::make_shared<PidCar>(nh);
             double dt = 0;
             nh.getParam("dt", dt);
+            nh.getParam("use_serial", use_serial_);
+            nh.getParam("port", port_);
+            nh.getParam("baudrate", baudrate_);
+
+            // serial use initialization
+            if (use_serial_)
+            {
+                int ret = 0;
+                ret = SerialInit();
+                assert(ret == 1);
+            }
 
             plan_timer_ = nh.createTimer(ros::Duration(dt), &PidNodelet::plan_timer_callback, this);
+            odom_sub_ = nh.subscribe<nav_msgs::Odometry>("odom", 1, &PidNodelet::odom_call_back, this);
         }
     };
 
