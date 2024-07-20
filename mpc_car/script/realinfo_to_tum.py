7eccf62cfcd02db9582cac9d75496745266d41a0
harry <harry@njust.edu.cn>
2024-07-20 14:53:06 +0800

    <ci>realifo_to_tum + reference

--- mpc_car/script/realinfo_to_tum.py
+++ mpc_car/script/realinfo_to_tum.py
@@ -4,6 +4,7 @@ import rospy
 from nav_msgs.msg import Path
 from omniGKF_control.msg import omniGKFinfo
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
 from tf.transformations import euler_from_quaternion
 import csv
 
@@ -12,6 +13,7 @@ import matplotlib.pyplot as plt
 # 全局变量，用于存储接收到的数据
 omniGKFinfo_data = []
 Odometry_data = []
+reference_path_data = []
 
 # 回调函数，处理接收到的数据
 # def omniGKFinfo_data_callback(msg):
@@ -23,6 +25,24 @@ Odometry_data = []
 #     global Odometry_data
 #     for pose in msg.poses:
 #         Odometry_data.append([msg.header.stamp.to_sec(),pose.pose.position.x, pose.pose.position.y])
+def reference_path_callback(msg):
+    pose = msg.pose
+    global reference_path_data
+    with open('reference_path.tum', 'a') as f_tum, open('reference_path.csv', 'a') as f_csv:
+        writer = csv.writer(f_csv)
+        data = [
+                msg.header.stamp.to_sec(), 
+                pose.position.x, 
+                pose.position.y, 
+                pose.position.z, 
+                pose.orientation.x, 
+                pose.orientation.y, 
+                pose.orientation.z, 
+                pose.orientation.w
+        ]
+        f_tum.write(f"{data[0]} {data[1]} {data[2]} {data[3]} {data[4]} {data[5]} {data[6]} {data[7]}\n")
+        writer.writerow(data)
+        reference_path_data.append(data)
 
 def Odometry_data_callback(msg):
     global Odometry_data
@@ -77,6 +97,7 @@ rospy.init_node('realinfo_data_get')
 # 订阅主题
 rospy.Subscriber('omniGKFinfo', omniGKFinfo, omniGKFinfo_data_callback)
 rospy.Subscriber('Odometry', Odometry, Odometry_data_callback)
+rospy.Subscriber('mpc_car/ref_state', PoseStamped, reference_path_callback)
 
 rospy.spin()
 
